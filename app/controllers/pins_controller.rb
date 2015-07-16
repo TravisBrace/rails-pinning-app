304c25d40eac92bbcbd69da5da90e22d9df6ff81
bractr <TravisBrace@gmail.com>
2015-07-15 21:01:52 -0400

    fixed new forms and added edit form

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -15,11 +15,38 @@ class PinsController < ApplicationController
   
   def new
     @pin = Pin.new
+    
   end
   
   def create
     @pin = Pin.create(pin_params)
-    render :index
+    
+    if @pin.valid?
+      @pin.save
+      redirect_to pin_path(@pin)
+    else
+      @errors = @pin.errors
+      render :new
+    end
+    
+  end
+  
+  def edit
+    @pin = Pin.find(params[:id])
+  end
+  
+  def update
+    @pin = Pin.find(params[:id])
+    
+    respond_to do |format|
+      if @pin.update(pin_params)
+        format.html { redirect_to @pin, notice: 'Beer was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
+      end
+    end
   end
   
   private
