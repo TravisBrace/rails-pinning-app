61b466c7144c6256f0f145c3ee2fa308bd24ed27
bractr <TravisBrace@gmail.com>
2015-07-19 13:28:15 -0400

    added paperclip and set config paths for heroku

--- Gemfile
+++ Gemfile
@@ -1,5 +1,10 @@
 source 'https://rubygems.org'
 
+gem 'paperclip'
+
+
+gem 'aws-sdk', '< 2.0'
+
 gem 'rails_12factor'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -52,7 +52,7 @@ class PinsController < ApplicationController
   private
   
   def pin_params
-    params.require(:pin).permit(:title, :url, :slug, :text, :resource_type, :category_id)
+    params.require(:pin).permit(:title, :url, :slug, :text, :resource_type, :category_id, :image)
   end
   
 end
