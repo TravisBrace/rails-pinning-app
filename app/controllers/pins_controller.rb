68d7617459f82c64d2b799480ad0c0a7e4348e18
bractr <TravisBrace@gmail.com>
2015-08-06 20:26:06 -0400

    another update for repin jawns

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -62,7 +62,7 @@ class PinsController < ApplicationController
   private
   
   def pin_params
-    params.require(:pin).permit(:title, :url, :slug, :text, :resource_type, :category_id, :image, :user_id, :pinning, :pinnings_attributes)
+    params.require(:pin).permit(:title, :url, :slug, :text, :resource_type, :category_id, :image, :user_id, :pinning)
   end
   
 end
