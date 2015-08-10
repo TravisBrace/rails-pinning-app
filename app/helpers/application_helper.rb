a2b7cad9268f18fc995c146c05ee5f0b42def54e
bractr <TravisBrace@gmail.com>
2015-08-10 18:18:45 -0400

    updated application help to include login specs for testing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  
+  def current_user
+    @user ||= User.where("id=?", session[:user_id]).first
+  end
+  helper_method :current_user
+  
+  def require_login
+    if !logged_in?
+      redirect_to :login
+    end
+  end
+  
+  def logged_in?
+    !current_user.nil? && !current_user.id.nil?
+  end
+  helper_method :logged_in?
+  
 end
