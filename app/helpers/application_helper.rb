b1f8ca07d545917f29e1617b52e7c2d42b55ce0f
bractr <TravisBrace@gmail.com>
2015-08-10 18:26:42 -0400

    had to fix that application helper more to come

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,2 @@
 module ApplicationHelper
-  
-  def current_user
-    @user ||= User.where("id=?", session[:user_id]).first
-  end
-  helper_method :current_user
-  
-  def require_login
-    if !logged_in?
-      redirect_to :login
-    end
-  end
-  
-  def logged_in?
-    !current_user.nil? && !current_user.id.nil?
-  end
-  helper_method :logged_in?
-  
 end
