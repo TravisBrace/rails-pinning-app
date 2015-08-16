f8a495cc04aab9b582f4169d78fcc306b91a3aca
bractr <TravisBrace@gmail.com>
2015-08-15 23:00:35 -0400

    started nav bar

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -2,13 +2,15 @@ var main = function () {
   $('.left').mouseenter(function() {
     $('.left').toggleClass('left-open');
     $('ul').toggle('li');
-    $('.right').toggleClass('right-open')
+    $('.right').toggleClass('right-open');
+    $('.tab').toggleClass('tab-open');
  });
   
   $('.left').mouseleave(function() {
     $('.left').toggleClass('left-open');
     $('ul').toggle('li');
     $('.right').toggleClass('right-open');
+    $('.tab').toggleClass('tab-open')
   });
 
 }
